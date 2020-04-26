0344ebd1c59bf4b2c0994ad686fd52f71fed6292
Mike <mike@>
2020-04-26 23:06:42 +0300

    6 02 HW5 profile test

--- src/test/java/ru/javawebinar/topjava/service/AbstractMealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/AbstractMealServiceTest.java
@@ -27,14 +27,7 @@ import static ru.javawebinar.topjava.MealTestData.*;
 import static ru.javawebinar.topjava.UserTestData.ADMIN_ID;
 import static ru.javawebinar.topjava.UserTestData.USER_ID;
 
-@ContextConfiguration({
-        "classpath:spring/spring-app.xml",
-        "classpath:spring/spring-db.xml"
-})
-@RunWith(SpringRunner.class)
-@Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
-@ActiveProfiles(resolver = ActiveDbProfileResolver.class)
-public class MealServiceTest {
+public abstract class AbstractMealServiceTest extends AbstractServiceTest {
     private static final Logger log = getLogger("result");
 
     private static StringBuilder results = new StringBuilder();
@@ -53,6 +46,12 @@ public class MealServiceTest {
         }
     };
 
+    @Autowired
+    protected MealService service;
+    @Autowired
+    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
+    private MealRepository repository;
+
     @AfterClass
     public static void printResult() {
         log.info("\n---------------------------------" +
@@ -61,9 +60,6 @@ public class MealServiceTest {
                 results +
                 "\n---------------------------------");
     }
-
-    @Autowired
-    private MealService service;
 
     @Test
     public void delete() throws Exception {
